b7ceec34f4a712e8a7d379abf100b5d5d8198822
Your Name <ryan.caney@sv.cmu.edu>
2011-09-25 17:56:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaneyWebsite::Application.config.secret_token = 'ecb7f054dfea684a26ae75b4171e7d30bad71a4832172ad93e21c8c8a53823abdaf73c877286519eb7d222724a994d372bccbe20aa4daa35a0788473cee30ff7'
